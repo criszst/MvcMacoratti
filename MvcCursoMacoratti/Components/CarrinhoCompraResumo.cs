ec35dcc9803e8e690ec61c10ed5f005b5e43f06d
Cristian <adrian.cristian.st@gmail.com>
2024-08-19 22:57:18 -0300

    VOLTEI IHUU

--- MvcCursoMacoratti/Components/CarrinhoCompraResumo.cs
+++ MvcCursoMacoratti/Components/CarrinhoCompraResumo.cs
@@ -1,6 +1,36 @@
-﻿namespace MvcMacorattiLanchesMac.Components
+﻿
+using Microsoft.AspNetCore.Mvc;
+using MvcMacorattiLanchesMac.ViewModels;
+using MvcMacorattiLanchesMac.Models;
+
+
+namespace MvcMacorattiLanchesMac.Components
 {
-    public class CarrinhoCompraResumo
+    public class CarrinhoCompraResumo : ViewComponent
     {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItems();
+
+
+            //var itens = new List<CarrinhoCompraItem>() {
+            //new CarrinhoCompraItem(),
+            //new CarrinhoCompraItem(),
+            //};
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+            return View(carrinhoCompraVM);
+        }
     }
 }
